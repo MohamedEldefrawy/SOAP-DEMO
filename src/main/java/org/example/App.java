4898fc5a93563d2d0a72a96a1d6b4df6b5aac21b
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-11-13 20:29:16 +0200

    User unit test to test SOAP

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,20 +1,14 @@
 package org.example;
 
-import org.example.CountryInfoService;
-
-import java.util.List;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
         CountryInfoService countryInfoService = new CountryInfoService();
         var countries = countryInfoService.getCountryInfoServiceSoap().listOfCountryNamesByCode().tCountryCodeAndName;
         for (var country: countries
-             ) {
+        ) {
             System.out.println(country.sName);
         }
     }
---
+++ src/test/java/CountryUnitTest.java
@@ -0,0 +1,25 @@
+import org.example.CountryInfoService;
+import org.example.CountryInfoServiceSoapType;
+import org.example.TCountryCodeAndName;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.List;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CountryUnitTest {
+    private CountryInfoServiceSoapType countryInfoServiceSoapType;
+
+    @BeforeAll
+    void initialize() {
+        this.countryInfoServiceSoapType = new CountryInfoService().getCountryInfoServiceSoap();
+    }
+
+    @Test
+    public void testListOfCountryNamesByCode() {
+        List<TCountryCodeAndName> countries = this.countryInfoServiceSoapType.listOfCountryNamesByCode().getTCountryCodeAndName();
+        Assertions.assertTrue(countries.size() > 0);
+    }
+}
