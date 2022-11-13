d2ff7f47f233e24176ecbf26a3d9a7cf5ee0ac39
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-11-13 21:12:49 +0200

    Add TextCasing WSDL

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,7 @@
 package org.example;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        CountryInfoService countryInfoService = new CountryInfoService();
-        var countries = countryInfoService.getCountryInfoServiceSoap().listOfCountryNamesByCode().tCountryCodeAndName;
-        for (var country: countries
-        ) {
-            System.out.println(country.sName);
-        }
+
     }
 }
